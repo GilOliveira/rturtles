68f818a84533566be630cdbf3430bac42ecf97a4
Gil Oliveira <giloliveira@alunos.fc.ul.pt>
2019-04-29 19:20:42 +0100

    used EventListeners for the modals and the menu

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 "use strict";
 
 function main() {
+
+    let botaoHamburguer = document.getElementById("hamburger");
+    let closeRegisterButton = document.getElementById("closeRegister");
+    let registerModal = document.getElementById("registerModal");
+    let loginModal = document.getElementById("loginModal");
+    let loginButton = document.getElementById('openLogin');
+    let registerButton = document.getElementById("openRegister");
+    let closeLoginButton = document.getElementById("closeLogin");
+    let mobileLoginButton = document.getElementById("openLoginMobile");
+    let mobileRegisterButton = document.getElementById("openRegisterMobile");
+
+    botaoHamburguer.addEventListener("click", openMenu);
+    closeRegisterButton.addEventListener("click", closeRegister);
+    loginButton.addEventListener("click", openLogin);
+    registerButton.addEventListener("click", openRegister);
+    closeLoginButton.addEventListener("click", closeLogin);
+    mobileLoginButton.addEventListener("click", openLogin);
+    mobileRegisterButton.addEventListener("click", closeLogin);
+
+    function openLogin() {
+        loginModal.style.display = 'block';
+    }
+
+    function closeLogin() {
+        loginModal.style.display = 'none';
+    }
+
+
+    function openRegister() {
+        registerModal.style.display = 'block';
+    }
+
+    function closeRegister() {
+        registerModal.style.display = 'none';
+    }
+
     function openMenu() {
         if (document.getElementById("menuLateral").style.display == "block") {
             document.getElementById("menuLateral").style.display = "none";
@@ -10,7 +46,6 @@ function main() {
         }
     }
     console.log('Estou aqui');
-    document.getElementById("hamburger").onclick = openMenu;
 }
 
 window.onload = main;
