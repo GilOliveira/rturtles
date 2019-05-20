a58b733cd44a25ac221098d8bc490d093b87602e
fistorres <sofia.storres@hotmail.com>
2019-05-20 19:59:11 +0100

    Butões para teste

--- move.js
+++ move.js
@@ -2,28 +2,12 @@
 
 function game(users,boardsize) {
 //    Users is a array of users to play
-
-    function order(users) {
-
-        var usersdic = {};
-        for (let i=0; i< users.length; i++); {
-            var bday = users[i].birth.replace("-","").replace("-","");
-            usersdic[parseInt(bday)] = users[i];
-        }
-        var lst = [];
-        for (var key in usersdic) {
-            lst.push(usersdic[key])
-        }
-        var ordered = insertion_sort(lst);
-        // this is the final list of ordered users
-        var orderesuser = [];
-        for (let i= 0; i < ordered.length; i++) {
-            orderesuser.push(usersdic[i])
-        }
-        }
-
-
+    var ordusers = order(users);
+    var currentplayes = ordusers[0];
+    var bugbutton = false;
+    var endturnbutton = false;
     }
+
 
 function insertion_sort(lst) {
     for (let i = 1; i < lst.length; i++) {
@@ -37,3 +21,24 @@ function insertion_sort(lst) {
         }
     return lst
     }
+
+function order(users) {
+
+    var usersdic = {};
+    for (let i=0; i< users.length; i++); {
+        var bday = users[i].birth.replace("-","").replace("-","");
+        usersdic[parseInt(bday)] = users[i];
+    }
+    var lst = [];
+    for (var key in usersdic) {
+        lst.push(usersdic[key])
+    }
+    var ordered = insertion_sort(lst);
+    // this is the final list of ordered users
+    var orderesuser = [];
+    for (let i= 0; i < ordered.length; i++) {
+        orderesuser.push(usersdic[i])
+    }
+    return orderesuser
+}
+}
